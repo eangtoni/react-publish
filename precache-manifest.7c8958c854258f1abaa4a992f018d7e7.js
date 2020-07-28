b371c1aa063f5d4428a6cdca9c5c68c7e7850e95
eangtoni <erik.angtoni88@gmail.com>
2020-07-28 23:48:42 +0800

    Updates

---
+++ precache-manifest.7c8958c854258f1abaa4a992f018d7e7.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "479c1e3b5232f982601cf30fa156dd18",
+    "url": "/react-publish/index.html"
+  },
+  {
+    "revision": "2df0655b871a0d0c99cf",
+    "url": "/react-publish/static/css/main.dbc51a20.chunk.css"
+  },
+  {
+    "revision": "9751aa795a0b327c055c",
+    "url": "/react-publish/static/js/2.0fb63ae3.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/react-publish/static/js/2.0fb63ae3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2df0655b871a0d0c99cf",
+    "url": "/react-publish/static/js/main.1d9845a9.chunk.js"
+  },
+  {
+    "revision": "2abc8c2864510046f6c1",
+    "url": "/react-publish/static/js/runtime-main.430a466a.js"
+  },
+  {
+    "revision": "676b68bd6e7091d79052707fec3d8803",
+    "url": "/react-publish/static/media/rosetta1.676b68bd.png"
+  },
+  {
+    "revision": "08ba3dce2f5b49b088a465daa021c083",
+    "url": "/react-publish/static/media/rosetta2.08ba3dce.png"
+  },
+  {
+    "revision": "37ca3b60c7563213506a2307c9b3e209",
+    "url": "/react-publish/static/media/tulip1.37ca3b60.png"
+  },
+  {
+    "revision": "501cdcd7751f0761478f0cf571528060",
+    "url": "/react-publish/static/media/tulip2.501cdcd7.png"
+  }
+]);
